ff57bc0233bdf956609ee18b12654c215d23ab69
Alberto Soragna <alberto.soragna@gmail.com>
2021-10-25 09:43:55 +0100

    add wombat-ws
    Signed-off-by: Alberto Soragna <alberto.soragna@gmail.com>

--- utils/dev-environment/.devdocker/run.sh
+++ utils/dev-environment/.devdocker/run.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 THIS_DIR=$(dirname $(realpath -s $0))
-WOMBAT_DIR=$(dirname ${THIS_DIR})
-CURR_RELATIVE_PATH=$(realpath --relative-to="${WOMBAT_DIR}" "${PWD}")
-echo $CURR_RELATIVE_PATH
-if [ "${CURR_RELATIVE_PATH##.}" != "${CURR_RELATIVE_PATH}" ]; then
-  CURR_RELATIVE_PATH=.
-fi
-
-echo $CURR_RELATIVE_PATH
 
 # Build the developer's dockerfile
 docker build \
@@ -29,17 +21,14 @@ if type "nvidia-container-cli" &> /dev/null && nvidia-container-cli info &> /dev
   GPU_FLAG="--gpus=all"
 fi
 
-# Check if this directory is in a workspace already
-# NOTE: this assumes that workspaces follow the `colcon` naming scheme
-#if [ $(basename $(dirname ${WOMBAT_DIR})) == "src" ]; then
-#  HOST_WOMBAT_WS_DIR=$(dirname $(dirname ${WOMBAT_DIR}))
-#else
-#  HOST_WOMBAT_WS_DIR=$WOMBAT_DIR
-#fi
-#DOCKER_WOMBAT_DIR_NAME=$(basename ${HOST_WOMBAT_WS_DIR})
-#DOCKER_WOMBAT_DIR="/workspaces/${DOCKER_WOMBAT_DIR_NAME}"
-
+# Define some useful directory names
+WOMBAT_DIR=$(dirname ${THIS_DIR})
 DOCKER_HOME=/home/docker-dev
+DOCKER_WOMBAT_WS=/workspaces/wombat_ws
+HOST_WOMBAT_WS=$(dirname ${WOMBAT_DIR})/wombat-ws
+
+# Make sure that host wombat workspace exists
+mkdir -p $HOST_WOMBAT_WS/src
 
 # Run the developer's dockerfile
 docker run -it --rm \
@@ -53,7 +42,8 @@ docker run -it --rm \
   --volume=${HOME}:${DOCKER_HOME}/host-home:rw \
   --volume=${HOME}/.gitconfig:${DOCKER_HOME}/.gitconfig:ro \
   --volume=${HOME}/.ssh:${DOCKER_HOME}/.ssh:ro \
-  #--volume=${HOST_WOMBAT_WS_DIR}:${DOCKER_WOMBAT_DIR} \
-  #--workdir ${DOCKER_WOMBAT_DIR}/${CURR_RELATIVE_PATH} \
+  --volume=${WOMBAT_DIR}:${DOCKER_WOMBAT_WS}/src/wombat:rw \
+  --volume=${HOST_WOMBAT_WS}:${DOCKER_WOMBAT_WS}:rw \
+  --workdir ${DOCKER_WOMBAT_WS} \
   wombat-dev \
   $@
