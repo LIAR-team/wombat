4886b748effeba1c47a1ad4ddaa2a7fe767c8b36
Alberto Soragna <alberto.soragna@gmail.com>
2022-10-15 12:18:18 +0100

    update CMake files
    use wombat_package() in wombat_cmake; mark linters as test dependencies; use wombat_package_install properly in wombat_strategy; minor fixes
    
    Signed-off-by: Alberto Soragna <alberto.soragna@gmail.com>

--- wombat_cmake/cmake/BuildTypes.cmake
+++ wombat_cmake/cmake/BuildTypes.cmake
@@ -6,15 +6,22 @@
 # https://gcc.gnu.org/onlinedocs/gcc-9.3.0/gcc/Optimize-Options.html#Optimize-Options
 
 # Release
-set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG" CACHE STRING "Flags used by the C++ compiler for builds optimized for speed." FORCE)
-set(CMAKE_C_FLAGS_RELEASE "-O3 -DNDEBUG" CACHE STRING "Flags used by the C compiler for builds optimized for speed." FORCE)
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG"
+  CACHE STRING "Flags used by the C++ compiler for builds optimized for speed." FORCE
+)
+set(CMAKE_C_FLAGS_RELEASE "-O3 -DNDEBUG"
+  CACHE STRING "Flags used by the C compiler for builds optimized for speed." FORCE
+)
 
 # RelWithDebug
-set(CMAKE_CXX_FLAGS_RELWITHDEBUG "-O2 -g -DDEBUG" CACHE STRING "Flags used by the C++ compiler for fast builds with full debug support." FORCE)
-set(CMAKE_C_FLAGS_RELWITHDEBUG "-O2 -g -DDEBUG" CACHE STRING "Flags used by the C compiler for fast builds with full debug support." FORCE)
+set(CMAKE_CXX_FLAGS_RELWITHDEBUG "-O2 -g -DDEBUG"
+  CACHE STRING "Flags used by the C++ compiler for fast builds with full debug support." FORCE
+)
+set(CMAKE_C_FLAGS_RELWITHDEBUG "-O2 -g -DDEBUG"
+  CACHE STRING "Flags used by the C compiler for fast builds with full debug support." FORCE
+)
 
-set(allowedBuildTypes "RELEASE;RELWITHDEBUG")
-set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "${allowedBuildTypes}")
+set(_allowedBuildTypes "RELEASE;RELWITHDEBUG")
 
 #
 # Makes sure that the CMAKE_BUILD_TYPE variable is properly set.
@@ -30,8 +37,8 @@ macro(handle_cmake_build_type)
     # Set default build type
     message(STATUS "Setting build type to RelWithDebug as none was specified.")
     set(CMAKE_BUILD_TYPE RELWITHDEBUG CACHE STRING "" FORCE)
-  elseif(NOT ${CMAKE_BUILD_TYPE} IN_LIST allowedBuildTypes)
+  elseif(NOT ${CMAKE_BUILD_TYPE} IN_LIST _allowedBuildTypes)
     # The provided type is not valid
-    message(FATAL_ERROR "Invalid build type: ${CMAKE_BUILD_TYPE}. Please select one of ${allowedBuildTypes}")
+    message(FATAL_ERROR "Invalid build type: ${CMAKE_BUILD_TYPE}. Please select one of ${_allowedBuildTypes}")
   endif()
 endmacro()
