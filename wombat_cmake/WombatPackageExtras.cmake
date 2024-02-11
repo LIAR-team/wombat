67dcad84558cced74d5a25d8850964378a0e2e64
Alberto Soragna <alberto.soragna@gmail.com>
2024-02-11 17:51:00 +0100

    automate creation of config-extras files in wombat_cmake

--- wombat_cmake/WombatPackageExtras.cmake
+++
@@ -1,6 +0,0 @@
-# Copyright 2021-2022 Soragna Alberto.
-# All Rights Reserved.
-# Unauthorized copying via any medium is strictly prohibited.
-# Proprietary and confidential.
-
-include("${wombat_cmake_DIR}/WombatPackage.cmake")
