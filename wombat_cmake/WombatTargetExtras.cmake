c3ce44e82cc4f09394654dd2fc1c32d50d847441
Alberto Soragna <alberto.soragna@gmail.com>
2024-02-11 03:04:06 +0100

    reduce cmake boilerplate

---
+++ wombat_cmake/WombatTargetExtras.cmake
@@ -0,0 +1,6 @@
+# Copyright 2021-2022 Soragna Alberto.
+# All Rights Reserved.
+# Unauthorized copying via any medium is strictly prohibited.
+# Proprietary and confidential.
+
+include("${wombat_cmake_DIR}/WombatTarget.cmake")
---
+++ wombat_cmake/cmake/WombatTarget.cmake
@@ -0,0 +1,25 @@
+# Copyright 2024 Soragna Alberto.
+# All Rights Reserved.
+# Unauthorized copying via any medium is strictly prohibited.
+# Proprietary and confidential.
+
+#
+# Add a CMake Library target and creates an alias
+# namespaced in the current project.
+#
+# For example, calling `wombat_add_library(my_lib STATIC)` in `MyProject`
+# will result in creating the library target `my_lib` and its alias `MyProject::my_lib`.
+#
+# @public
+#
+function(wombat_add_library LIBRARY_NAME LIBRARY_TYPE)
+  if(NOT LIBRARY_NAME)
+    message(FATAL_ERROR "Argument LIBRARY_NAME is null")
+  endif()
+  if(NOT LIBRARY_TYPE)
+    message(FATAL_ERROR "Argument LIBRARY_TYPE is null")
+  endif()
+
+  add_library(${LIBRARY_NAME} ${LIBRARY_TYPE})
+  add_library(${PROJECT_NAME}::${LIBRARY_NAME} ALIAS ${LIBRARY_NAME})
+endfunction()
